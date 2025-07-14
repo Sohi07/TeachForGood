a7ad8e5302b4f51ba659657a982539f3cf6e2749
Sohini Das <sohinid474@gmail.com>
2025-07-15 02:56:59 +0530

    changes made on scrollToTop

--- src/components/useScrollToTop.js
+++ src/components/useScrollToTop.js
@@ -9,11 +9,10 @@ const useScrollToTop = () => {
       });
     };
 
-    // Trigger the scroll to top when the component mounts
     scrollToTop();
 
-    // No cleanup needed as we're only scrolling on mount
-  }, []); // Empty dependency array ensures this effect runs only once on mount
+    
+  }, []); 
 };
 
 export default useScrollToTop;
